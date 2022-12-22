65733c69e94a43bc80e525ba1311877fbca9c921
Fraud47 <sojandem@gmail.com>
2022-12-22 07:40:30 +0200

    blank app

---
+++ gudum/src/main/java/com/zw/gudum/GudumApplication.java
@@ -0,0 +1,13 @@
+package com.zw.gudum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GudumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GudumApplication.class, args);
+	}
+
+}
---
+++ gudum/src/test/java/com/zw/gudum/GudumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zw.gudum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GudumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
